5792fae6da5d38b6bee2f806575f0b408e039dd7
julien <julien.fruteau@gmail.com>
2020-06-12 07:25:43 +0200

    Creating Enemy Laser

---
+++ Assets/Scripts/EnemyLaser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLaser : Laser
+{
+    
+    private void Start()
+    {
+        _direction = Vector3.down;
+        _speed = 12f;
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (other.tag)
+        {
+            case "Player":
+                Destroy(this.gameObject);
+                break;
+        }
+    }
+}
--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-
-    private Vector3 _direcion = Vector3.up;
-
+    protected Vector3 _direction = Vector3.up;
     [SerializeField]
-    private float _speed = 10f;
-
-    [SerializeField]
-    private float _yLimit = 7f;
-
+    protected float _speed = 10f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(_direcion * _speed * Time.deltaTime);
+        transform.Translate(_direction * _speed * Time.deltaTime);
 
-        if (transform.position.y > _yLimit)
+        if (transform.position.y > SpawnObjConst.yMax || transform.position.y < SpawnObjConst.yMin)
         {
             if (transform.parent != null)
             {
                 Destroy(transform.parent.gameObject);
             }
             Destroy(this.gameObject);
+        }
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (other.tag)
+        {
+            case "Enemy":
+                Destroy(this.gameObject);
+                break;
         }
     }
 }
